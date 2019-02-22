22f8f1cd0d780c8dcc71f6a93dce2e9d14c151c3
rubeeengm <rubeeengm@example.com>
2019-02-21 21:24:15 -0600

    cambio de nombre de app, zona horaria y mensajes de error en español

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace LaraDex\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Broadcast;
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace LaraDex\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
