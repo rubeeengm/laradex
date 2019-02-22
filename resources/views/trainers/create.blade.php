dd22407fe535782cb591fc81f1f3be6c9faa709c
rubeeengm <rubeeengm@example.com>
2019-02-21 22:24:23 -0600

    el metodo create ahora retorna una vista, creacion de la vista create

---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="form-group">
+				<label for="">Nombre</label>
+				<input type="text" class="form-control">
+			</div>
+
+			<button type="submit" class="btn btn-primary">Guardar</button>
+		</div>
+	</body>
+</html>
