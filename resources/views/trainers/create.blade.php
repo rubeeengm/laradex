67de0d1f92828c60bad7c7db7e7252e12c59bc51
rubeeengm <rubeeengm@example.com>
2019-02-22 03:23:08 -0600

    se crea una subview form para reutilizar codigo

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -4,22 +4,7 @@
 
 @section('content')
 	{!! Form::open(['route' => 'trainers.store', 'method' =>'POST', 'files' => true]) !!}
-		<div class="form-group">
-			{!! Form::label('name', 'Nombre') !!}
-			{!! Form::text('name',null,['class' => 'form-control']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::label('slug', 'Slug') !!}
-			{!! Form::text('slug',null,['class' => 'form-control']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::label('avatar', 'Avatar') !!}
-			{!! Form::file('avatar') !!}
-		</div>
-
-
+		@include('trainers.form')
 		{!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
 	{!! Form::close() !!}
 
--- resources/views/trainers/edit.blade.php
+++ resources/views/trainers/edit.blade.php
@@ -11,20 +11,7 @@
 		])
 	!!}
 		
-		<div class="form-group">
-			{!! Form::label('name', 'Nombre') !!}
-			{!! Form::text('name',null,['class' => 'form-control']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::label('slug', 'Slug') !!}
-			{!! Form::text('slug',null,['class' => 'form-control']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::label('avatar', 'Avatar') !!}
-			{!! Form::file('avatar') !!}
-		</div>
+		@include('trainers.form')
 
 		{!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
 	{!! Form::close() !!}
