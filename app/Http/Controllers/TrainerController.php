c6bacd347be687bbf256b3e376143c6e0bda3304
rubeeengm <rubeeengm@example.com>
2019-02-22 01:32:58 -0600

    se crea la migracion para el campo avatar, se modifica store en trainer controller y se modifican el index y create de trainer

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -37,8 +37,15 @@ class TrainerController extends Controller
      */
     public function store(Request $request)
     {
+        if ($request -> hasFile('avatar')) {
+            $file = $request -> file('avatar');
+            $name = time().$file->getClientOriginalName();
+            $file -> move(public_path().'/images/', $name);
+        }
+
         $trainer = new Trainer();
         $trainer->name = $request->input('name');
+        $trainer->avatar = $name;
         $trainer->save();
 
         return "Saved";
--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -3,12 +3,17 @@
 @section('title','Trainers Create')
 
 @section('content')
-	<form action="/trainers" method="POST" class="form-group">
+	<form action="/trainers" method="POST" class="form-group" enctype="multipart/form-data">
 		@csrf
 		<div class="container">
 			<div class="form-group">
 				<label for="">Nombre</label>
 				<input type="text" name="name" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Avatar</label>
+				<input type="file" name="avatar">
 			</div>
 
 			<button type="submit" class="btn btn-primary">Guardar</button>
