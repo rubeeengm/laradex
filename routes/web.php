601d1a4c79e89372eea7e20b7c03ae8331c0d578
rubeeengm <garciamalagaruben@gmail.com>
2019-03-06 03:02:13 -0600

    creacion de modulo de autenticacion usando php artisan

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,6 @@ Route::get('/name/{name}/lastname/{lastname?}', function($name, $lastname = null
 Route::get('prueba/{parametro}', 'PruebaController@prueba');
 
 Route::resource('trainers', 'TrainerController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
