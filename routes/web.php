f5ff1ca40460ec756b12b45c500701808858f521
rubeeengm <rubeeengm@example.com>
2019-03-15 01:56:32 -0600

    creacion de pokemonController, se agregó al archivo de rutas, creacion de vista pokemons.index, y modificacion de exampleComponent, instalación de vue

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::get('/name/{name}/lastname/{lastname?}', function($name, $lastname = null
 Route::get('prueba/{parametro}', 'PruebaController@prueba');
 
 Route::resource('trainers', 'TrainerController');
+Route::resource('pokemons', 'PokemonController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
